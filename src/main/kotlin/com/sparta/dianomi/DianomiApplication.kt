4c151d89cfa979ad80579701dd3e07ac4a56e199
zunbee <zunbeeok@gmail.com>
2024-01-22 15:25:01 +0900

    Init : "설정 dianomi 프로젝트 셋팅"

---
+++ src/main/kotlin/com/sparta/dianomi/DianomiApplication.kt
@@ -0,0 +1,11 @@
+package com.sparta.dianomi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DianomiApplication
+
+fun main(args: Array<String>) {
+	runApplication<DianomiApplication>(*args)
+}
