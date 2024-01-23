aa5571e6ddaa3fa20f870a37ac2e7acb63b16679
zunbee <zunbeeok@gmail.com>
2024-01-23 11:50:45 +0900

    Feat : "스프링 시큐리티 - Jwt, 인증/인가 구현"

---
+++ src/main/kotlin/com/sparta/dianomi/authority/jwt/JwtAuthenticationToken.kt
@@ -0,0 +1,26 @@
+package com.sparta.dianomi.authority.jwt
+
+import com.sparta.dianomi.authority.security.UserPrincipal
+import org.springframework.security.authentication.AbstractAuthenticationToken
+import org.springframework.security.web.authentication.WebAuthenticationDetails
+
+class JwtAuthenticationToken(
+    private val principal: UserPrincipal,
+    details: WebAuthenticationDetails
+): AbstractAuthenticationToken(principal.authorities) {
+
+    init {
+        super.setAuthenticated(true)
+        super.setDetails(details)
+    }
+
+    override fun getCredentials() = null
+
+
+    override fun getPrincipal() = principal
+
+    override fun isAuthenticated(): Boolean {
+        return true
+    }
+}
+
