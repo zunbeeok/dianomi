dc439512f46c095ba240c20bcec015a2e67ee21b
Karox1234 <zzzcomet@gmail.com>
2024-01-23 20:38:33 +0900

    Feat : Entrypoint,handler 추가

---
+++ src/main/kotlin/com/sparta/dianomi/authority/security/CustomAuthenticationEntrypoint.kt
@@ -0,0 +1,28 @@
+package com.sparta.dianomi.authority.security
+
+import com.sparta.dianomi.common.exception.dto.ErrorResponse
+import com.fasterxml.jackson.databind.ObjectMapper
+import jakarta.servlet.http.HttpServletRequest
+import jakarta.servlet.http.HttpServletResponse
+import org.springframework.http.MediaType
+import org.springframework.security.core.AuthenticationException
+import org.springframework.security.web.AuthenticationEntryPoint
+import org.springframework.stereotype.Component
+
+@Component
+class CustomAuthenticationEntrypoint: AuthenticationEntryPoint {
+
+    override fun commence(
+        request: HttpServletRequest,
+        response: HttpServletResponse,
+        authException: AuthenticationException
+    ) {
+        response.status = HttpServletResponse.SC_UNAUTHORIZED
+        response.contentType = MediaType.APPLICATION_JSON_VALUE
+        response.characterEncoding = "UTF-8"
+
+        val objectMapper = ObjectMapper()
+        val jsonString = objectMapper.writeValueAsString(ErrorResponse("JWT 인증 실패"))
+        response.writer.write(jsonString)
+    }
+}
