375f3279895b6e98ea0efe918ee46b74410a4af4
Karox1234 <zzzcomet@gmail.com>
2024-01-23 14:33:07 +0900

    Fix : sign_up 오타 수정

--- src/main/kotlin/com/sparta/dianomi/authority/security/SecurityConfig.kt
+++ src/main/kotlin/com/sparta/dianomi/authority/security/SecurityConfig.kt
@@ -1,21 +1,46 @@
 package com.sparta.dianomi.authority.security
 
+import com.sparta.dianomi.authority.jwt.JwtAuthenticationFilter
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.web.AuthenticationEntryPoint
 import org.springframework.security.web.SecurityFilterChain
+import org.springframework.security.web.access.AccessDeniedHandler
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
 
 @Configuration
 @EnableWebSecurity
-class SecurityConfig {
-    //꺼야 되는 필터들을 꺼주는 과정
+@EnableMethodSecurity
+class SecurityConfig(
+    private val jwtAuthenticationFilter: JwtAuthenticationFilter,
+    private val authenticationEntrypoint: AuthenticationEntryPoint,
+    private val accessDeniedHandler: AccessDeniedHandler
+) {
+
     @Bean
     fun filterChain(http: HttpSecurity): SecurityFilterChain {
         return http
-            .httpBasic{it.disable()}
-            .formLogin{it.disable()}
-            .csrf{it.disable()}
+            .httpBasic { it.disable() }
+            .formLogin { it.disable() }
+            .csrf { it.disable() }
+            .authorizeHttpRequests {
+                it.requestMatchers(
+                    "/member/login",
+                    "/member/sign_up",
+                    "/swagger-ui/**",
+                    "/v3/api-docs/**"
+                ).permitAll()
+                    .anyRequest().authenticated()
+            }
+            .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter::class.java)
+            .exceptionHandling{
+                it.authenticationEntryPoint(authenticationEntrypoint)
+                it.accessDeniedHandler(accessDeniedHandler)
+            }
             .build()
     }
+
 }
