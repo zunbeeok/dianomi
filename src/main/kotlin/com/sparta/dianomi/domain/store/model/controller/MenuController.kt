0756bd311ab482f7c2ef4a6867223614ce63d2a4
홍성욱 <suh75321@naver.com>
2024-01-24 16:15:20 +0900

    Feat : "Menu의 인증인가 업로드 PreAuthorize는 뺴면 제대로 안되는거 같아서 넣었습니다 "

--- src/main/kotlin/com/sparta/dianomi/domain/store/model/controller/MenuController.kt
+++ src/main/kotlin/com/sparta/dianomi/domain/store/model/controller/MenuController.kt
@@ -1,6 +1,7 @@
 package com.sparta.dianomi.domain.store.model.controller
 
 
+import com.sparta.dianomi.authority.security.UserPrincipal
 import com.sparta.dianomi.domain.store.model.dto.MenuCreateDto
 import com.sparta.dianomi.domain.store.model.dto.MenuDto
 import com.sparta.dianomi.domain.store.model.dto.MenuUpdateDto
@@ -12,6 +13,7 @@ import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
 import org.springframework.security.access.prepost.PreAuthorize
+import org.springframework.security.core.annotation.AuthenticationPrincipal
 import org.springframework.web.bind.annotation.*
 
 @RestController
@@ -20,9 +22,10 @@ import org.springframework.web.bind.annotation.*
 class MenuController(private val menuService: MenuService) {
 
     @Operation(summary = "메뉴 정보 업데이트", description = "주어진 메뉴 ID에 해당하는 메뉴의 정보를 업데이트합니다.")
-    //@PreAuthorize("hasRole('ADMIN')")
+    @PreAuthorize("hasRole('STORE')")
     @PutMapping("/{menuId}", produces = [MediaType.APPLICATION_JSON_VALUE])
     fun updateMenu(
+        @AuthenticationPrincipal user: UserPrincipal,
         @Parameter(description = "스토어 ID") @PathVariable storeId: Long,
         @Parameter(description = "메뉴 ID") @PathVariable menuId: Long,
         @Parameter(description = "업데이트할 메뉴 정보") @RequestBody menuUpdateDTO: MenuUpdateDto
@@ -32,9 +35,10 @@ class MenuController(private val menuService: MenuService) {
     }
 
     @Operation(summary = "새 메뉴 생성", description = "새로운 메뉴를 생성합니다.")
-    //@PreAuthorize("hasRole('ADMIN')")
+    @PreAuthorize("hasRole('STORE')")
     @PostMapping(produces = [MediaType.APPLICATION_JSON_VALUE])
     fun createMenu(
+        @AuthenticationPrincipal user: UserPrincipal,
         @Parameter(description = "스토어 ID") @PathVariable storeId: Long,
         @Parameter(description = "생성할 메뉴 정보") @RequestBody menuCreateDto: MenuCreateDto
     ): ResponseEntity<MenuDto> {
@@ -43,9 +47,10 @@ class MenuController(private val menuService: MenuService) {
     }
 
     @Operation(summary = "메뉴 삭제", description = "주어진 메뉴 ID에 해당하는 메뉴를 삭제합니다.")
-    //@PreAuthorize("hasRole('ADMIN')")
+    @PreAuthorize("hasRole('STORE')")
     @DeleteMapping("/{menuId}", produces = [MediaType.TEXT_PLAIN_VALUE])
     fun deleteMenu(
+        @AuthenticationPrincipal user: UserPrincipal,
         @Parameter(description = "스토어 ID") @PathVariable storeId: Long,
         @Parameter(description = "메뉴 ID") @PathVariable menuId: Long
     ): ResponseEntity<out Any> {
