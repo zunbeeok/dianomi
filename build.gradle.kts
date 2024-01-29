c0f4cae35733ecf724db7637bf34776a69aac6f2
Karox1234 <zzzcomet@gmail.com>
2024-01-29 11:01:13 +0900

    commit

--- build.gradle.kts
+++ build.gradle.kts
@@ -55,7 +55,7 @@ dependencies {
 	implementation("io.jsonwebtoken:jjwt-api:0.12.3")
 	runtimeOnly("io.jsonwebtoken:jjwt-impl:0.12.3")
 	runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.12.3")
-	//implementation("com.h2database:h2")
+//	implementation("com.h2database:h2")
 	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.2.0")
 }
 
